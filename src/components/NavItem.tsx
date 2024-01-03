c5d274a5fd556d2ed244225a3ac814b4eb18dad4
vaidik <vaidiksinghirwan@gamil.com>
2024-01-04 03:43:21 +0530

    added the add to cart ui in home page and also designed the some part of navbar for laptop screens

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -31,8 +31,8 @@ const NavItem = ({isAnyOpen,category,handleOpen,isOpen}:NavItemProps) => {
             </Button>
         </div>
         {isOpen ? (
-            <div className={cn("absolute inset-x-0 top-full text-sm text-muted-foreground",{
-                "animate-in fade-in-10 slide-in-from-top-5":!isAnyOpen
+            <div className={cn("  absolute inset-x-0 top-full text-sm text-muted-foreground",{
+                "animate-in  fade-in-10 slide-in-from-top-5 ":!isAnyOpen
             })}>
                 <div className='absolute inset-0 top-1/2 bg-white shadow' aria-hidden='true'/>
 
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,36 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import NavItem from "./NavItem"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside"
 
 const NavItems = () => {
     const [activeIndex,setActiveIndex] = useState<null | number>(null)
+    const navRef = useRef<HTMLDivElement | null>(null)
 
 
     const isAnyOpen = activeIndex !== null
+
+    useEffect(() => {
+        const handler = (e:KeyboardEvent) => {
+            if(e.key === "Escape") {
+                setActiveIndex(null)
+            }
+        }
+
+        document.addEventListener('keydown',handler)
+
+
+        return () => {
+            document.removeEventListener("keydown",handler)
+        } 
+    },[])
+
+    useOnClickOutside(navRef, () => setActiveIndex(null))
+
   return (
-    <div className="flex gap-4 h-full">
+    <div ref={navRef} className="flex gap-4 h-full">
         {
             PRODUCT_CATEGORIES.map((category,i) => {
                 const handleOpen = () => {
